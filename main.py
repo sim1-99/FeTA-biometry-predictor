712a22513f1e2e1d33ab2f3aeca68e771c115f47
sim1-99 <simonechiarella99@gmail.com>
2024-07-01 13:01:33 +0200

    Implement top-down architecture in main.py

--- main.py
+++ main.py
@@ -19,10 +19,37 @@ The seven input labels required as input are:
 7. brainstem (BS)
 
 The five predicted biometric measures are:
-1) brain biparietal diameter (bBIP_ax)
-2) skull biparietal diameter (sBIP_ax)
-3) transverse cerebellar diameter (TCD_cor)
+1) brain biparietal diameter (bBIP)
+2) skull biparietal diameter (sBIP)
+3) transverse cerebellar diameter (TCD)
 4) height of the vermis (HV)
 5) length of the corpus callosum (LCC)
 
 """
+import ax_meas_pred
+import cor_meas_pred
+import sag_meas_pred
+import evaluate
+from modules.utils import (
+    load_image,
+    load_meas,
+)
+
+# load data
+test_mask = load_image("~/sub-001_rec-mial_dseg.nii.gz")
+meas_gt = load_meas("~/biometry.tsv")
+
+# predict
+ax_meas = ax_meas_pred(test_mask)
+TCD_pred = cor_meas_pred(test_mask)
+sag_meas = sag_meas_pred(test_mask)
+
+bBip_pred = ax_meas[0]
+sBip_pred = ax_meas[1]
+HV_pred = sag_meas[0]
+LCC_pred = sag_meas[1]
+
+meas_preds = [bBip_pred, sBip_pred, TCD_pred, HV_pred, LCC_pred]
+
+#evaluate
+evaluate(meas_preds, meas_gt)
