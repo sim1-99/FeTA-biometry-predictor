d9bfaf79db42fb3b0df44d2f15c9ac614ed997d6
sim1-99 <simonechiarella99@gmail.com>
2024-07-02 19:20:29 +0200

    Changes in docstrings

--- evaluate.py
+++ evaluate.py
@@ -4,7 +4,10 @@ Copyright (c) 2024 Simone Chiarella
 Author: S. Chiarella
 Date: 2024-07-01
 
-This script evaluates the predictions of the biometric measurements.
+Evaluate the performance of the pipeline by comparing the biometric
+measurements extrcated from the segmented labels with the ground truth
+measurements. The evaluation is based on the computation of the mean absolute
+error (MAE) the mean squared error (MSE), and the R2 score.
 
 """
 import numpy as np
@@ -23,14 +26,14 @@ def main(
     store_path: str,
 ) -> None:
     """
-    Run and store the evaluation of the biometric measurement predictions.
+    Run and store the evaluation of the extracted biometric measurements.
 
     Parameters
     ----------
     meas_gt : pd.DataFrame
         The ground truth biometric measurements.
     meas_preds : np.ndarray
-        The predicted biometric measurements.
+        The extracted biometric measurements.
     store_path : str
         The path to the .txt file to store the evaluation results.
 
